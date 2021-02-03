b815f1748396f41c69508b7631cbbdc75ae5582e
susanna00 <77734618+susanna00@users.noreply.github.com>
2021-02-03 18:20:37 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,31 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+import random 
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+#
+# Asking user for an input
+#
+
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': rock")
+
+print(f"You chose: {user_choice}")
+
+#
+# Validate user input
+#
+
+#
+# Simulating a computer input 
+#
+
+
+#
+# Determining who won
+#
+
+
+
